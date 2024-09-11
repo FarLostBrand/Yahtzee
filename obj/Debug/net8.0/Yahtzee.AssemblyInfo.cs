0b7dfe93fec5b8c286f13ba819a78ab8fffe8240
FarLostBrand <lostbrand@outlook.com>
2024-09-11 18:00:30 -0400

    Yahtzee push

--- obj/Debug/net8.0/Yahtzee.AssemblyInfo.cs
+++ obj/Debug/net8.0/Yahtzee.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yahtzee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045397258316714ff708b35ba73600b171f8023e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yahtzee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yahtzee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
